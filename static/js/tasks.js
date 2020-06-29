f9d240601cc6e3190850130443758dcb68c89c29
EmaCrzz <ema_villanueva@hotmail.com>
2020-06-29 19:25:08 -0300

    countdown task

--- static/js/tasks.js
+++ static/js/tasks.js
@@ -11,14 +11,41 @@ class Task {
   constructor(props) {
     this.element = props.htmlElement,
     this.name= props.name,
-    this.dateInit = new Intl.DateTimeFormat('default', optionsDate).format(new Date()),
-    this.dateFinish = new Intl.DateTimeFormat('default', optionsDate).format(new Date(props.dateFinish)),
+    this.dateInit = new Date(),
+    this.dateFinish = new Date(props.dateFinish),
     this.color = props.color
+    this.id = "_" + Math.random().toString(36).substr(2, 9);
   }
 
+  countDown(){
+    var countDownDate = this.dateFinish.getTime();
+    var id = this.id
+
+    return setInterval(function () {
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor(
+        (distance % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById(`timer${id}`).innerHTML = "EXPIRED";
+      }
+
+      document.getElementById(`timer${id}`).innerHTML =
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    }, 1000);
+  }
 
   create() {
     const htmlItem = this.newTaskItem()
+    this.countDown()
     this.element.insertAdjacentHTML("beforeend", htmlItem);
   }
 
@@ -27,13 +54,17 @@ class Task {
       <li class="tasks-item is-${this.color}">
         <div class="tasks-info">
           <div>
-            <span class="tasks-name u-h5">${this.name}</span>
-            <span class="tasks-created">Created ${this.dateInit}</span>
+            <span class="tasks-name u-h4">${this.name}</span>
+            <span class="tasks-dates">Created ${new Intl.DateTimeFormat(
+              "default",
+              optionsDate
+            ).format(this.dateInit)}</span>
+            <span class="tasks-dates">Finish ${new Intl.DateTimeFormat(
+              "default",
+              optionsDate
+            ).format(this.dateFinish)}</span>
           </div>
-          <div>
-            <span class="tasks-finish-label">Finish</span>
-            <span class="tasks-finish-timer u-h5">${this.dateFinish}</span>
-          </div>
+          <span class="u-h4" id="timer${this.id}">Starting!</span>            
         </div>
         <button class="u-button with-icon is-outlined is-small">
           <img src="./static/icons/cerrar.svg" alt="">
